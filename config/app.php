b57906580350cb8e8e25dfcd6d3bcb04870d4521
ThangNguyen <nguyen.thang@alliedtechcamp.com>
2020-05-19 14:50:42 +0700

    Add config and driver

--- config/app.php
+++ config/app.php
@@ -175,7 +175,9 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        //Add
         App\Providers\ViewServiceProvider::class,
+        App\Providers\BroadcastServiceProvider::class,
 
     ],
 

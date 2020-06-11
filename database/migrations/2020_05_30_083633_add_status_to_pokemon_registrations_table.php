b3b5e3bd6badd49d68ac037779080073ca65a6c9
ThangNguyen <nguyen.thang@alliedtechcamp.com>
2020-06-11 08:44:06 +0700

    fix error migrate

--- database/migrations/2020_05_30_083633_add_status_to_pokemon_registrations_table.php
+++ database/migrations/2020_05_30_083633_add_status_to_pokemon_registrations_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-classAddStatusToPokemonRegistrationsTable extends Migration
+class AddStatusToPokemonRegistrationsTable extends Migration
 {
     /**
      * Run the migrations.

6b6a6af9e1b1aa5cf7d53c530d65fb99f0021d9b
ThangNguyen <thangnq95@gmail.com>
2020-06-13 09:45:18 +0700

    Change default value

--- database/migrations/2020_05_27_072926_create_pokemon_registrations_table.php
+++ database/migrations/2020_05_27_072926_create_pokemon_registrations_table.php
@@ -15,17 +15,17 @@ class CreatePokemonRegistrationsTable extends Migration
     {
         Schema::create('pokemon_registrations', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->string('no',5)->default(null);
-            $table->string('pokemon_name',50)->after('channel_id')->default(null);
+            $table->string('no',5)->nullable();
+            $table->string('pokemon_name',50)->default("");
             $table->string('discord_user_id');
-            $table->string('discord_user_name')->after('discord_user_id')->default(null);
+            $table->string('discord_user_name')->nullable();
             $table->string('channel_id');
             $table->string('channel_name',50);
-            $table->string('country', 50)->after('pokemon_name')->default("");
-            $table->integer('iv')->after('country')->default(0);
-            $table->integer('cp')->after('iv')->default(0);
-            $table->integer('level')->after('cp')->default(0);
-            $table->boolean('status')->after('channel_name')->default(1);
+            $table->string('country', 50)->default("");
+            $table->integer('iv')->default(0);
+            $table->integer('cp')->default(0);
+            $table->integer('level')->default(0);
+            $table->boolean('status')->default(1);
 
             $table->timestamps();
         });

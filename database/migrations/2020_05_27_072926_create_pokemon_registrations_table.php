15de662bb1d1ad6322a7f8cadd295098792ec47e
ThangNguyen <thangnq95@gmail.com>
2020-06-13 08:46:26 +0700

    Clean code

--- database/migrations/2020_05_27_072926_create_pokemon_registrations_table.php
+++ database/migrations/2020_05_27_072926_create_pokemon_registrations_table.php
@@ -15,10 +15,18 @@ class CreatePokemonRegistrationsTable extends Migration
     {
         Schema::create('pokemon_registrations', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->string('no',5);
+            $table->string('no',5)->default(null);
+            $table->string('pokemon_name',50)->after('channel_id')->default(null);
             $table->string('discord_user_id');
+            $table->string('discord_user_name')->after('discord_user_id')->default(null);
             $table->string('channel_id');
             $table->string('channel_name',50);
+            $table->string('country', 50)->after('pokemon_name')->default("");
+            $table->integer('iv')->after('country')->default(0);
+            $table->integer('cp')->after('iv')->default(0);
+            $table->integer('level')->after('cp')->default(0);
+            $table->boolean('status')->after('channel_name')->default(1);
+
             $table->timestamps();
         });
     }
